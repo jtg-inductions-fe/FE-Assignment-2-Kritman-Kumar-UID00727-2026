23e5b6b29aaf71c46d9b44dad075a1df077d30ae
kritmanraojtg <kritman.rao@joshtechnologygroup.com>
2026-07-16 14:03:34 +0530

    [KR_A2_01]: fix typography font weight, and line height

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
