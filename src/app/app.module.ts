859ac043b250486b2b7881ce5b07e15937a6051a
kritmanraojtg <kritman.rao@joshtechnologygroup.com>
2026-07-27 05:26:31 +0530

    [KR_A2_01]: fixed node version, .gitignore environment, app module import, breakpoint, and color file theme typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatIconModule } from '@angular/material/icon';
-import { AppComponent } from './app.component';
 import { MatListModule } from '@angular/material/list';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
