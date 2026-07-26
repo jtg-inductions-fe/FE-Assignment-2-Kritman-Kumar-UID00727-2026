fc8aa0ca574ceea706c210e6c0c739b414b214d4
kritmanraojtg <kritman.rao@joshtechnologygroup.com>
2026-07-26 15:04:47 +0530

    [KR_A2_01]: fixed PR comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule, MatButtonModule],
+  imports: [BrowserModule, MatListModule, MatIconModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
