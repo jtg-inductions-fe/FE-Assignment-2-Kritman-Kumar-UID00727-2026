a9525427b535eea7e23dbc63f48ecd4592711239
kritmanraojtg <kritman.rao@joshtechnologygroup.com>
2026-07-28 01:15:49 +0530

    [KR_A2_07]: add footer-config file. and improve footer template and styles

---
+++ src/app/shared/components/footer/footer.config.ts
@@ -0,0 +1,24 @@
+export const SOCIAL_LINKS_CONFIG = [
+  {
+    name: 'facebook',
+    logoPath: 'assets/logos/facebook.svg',
+    url: 'https://www.facebook.com',
+  },
+  {
+    name: 'twitter',
+    logoPath: 'assets/logos/x.svg',
+    url: 'https://www.x.com',
+  },
+  {
+    name: 'github',
+    logoPath: 'assets/logos/github.svg',
+    url: 'https://www.github.com',
+  },
+  {
+    name: 'wikipedia',
+    logoPath: '/assets/logos/internet.svg',
+    url: 'https://www.wikipedia.com',
+  },
+];
+
+export const FOOTER_COPYRIGHT_TEXT = 'Themesberg, LLC. All rights reserved.';
